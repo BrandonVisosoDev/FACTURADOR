f4b2bb820a7bfee61eccdb56940301cb8aa2146e
EDEN <kyudaeu2099@gmail.com>
2026-07-01 11:09:20 -0600

    Termino: Modelado de datos, creacion de modelos y pruebas con el panel admin de Django

--- facturas/admin.py
+++ facturas/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+# pyrefly: ignore [missing-import]
+from .models import Cliente, Producto, Factura, DetalleFactura
+
+# Configuración para ver los artículos dentro de la misma vista de la factura
+class DetalleFacturaInline(admin.TabularInline):
+    model = DetalleFactura
+    extra = 1  # Te muestra un renglón vacío por defecto para agregar un artículo
+
+@admin.register(Factura)
+class FacturaAdmin(admin.ModelAdmin):
+    list_display = ('numero_factura', 'cliente', 'fecha_emision', 'condicion_pago')
+    search_fields = ('numero_factura', 'cliente__nombre')
+    inlines = [DetalleFacturaInline]
+
+# Registros simples para tus catálogos
+admin.site.register(Cliente)
+admin.site.register(Producto)
